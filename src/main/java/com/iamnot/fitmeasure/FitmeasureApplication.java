a2e98b42ace52e67ac098c97702430a254f61de6
You Dong Kwan <dkyou7@naver.com>
2026-09-02 17:49:32 +0900

    chore: Spring Boot 4.1.1 / Java 21 기반 프로젝트 초기화
    - webmvc, thymeleaf, data-jpa, security, validation 스타터 구성
    - 로컬(H2) / 운영(MYSQL) 프로파일 분리
    - open-in-view 비활성화

---
+++ src/main/java/com/iamnot/fitmeasure/FitmeasureApplication.java
@@ -0,0 +1,13 @@
+package com.iamnot.fitmeasure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitmeasureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitmeasureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iamnot/fitmeasure/FitmeasureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iamnot.fitmeasure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitmeasureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
